1af80f3d40a111297f3d6c7de057ac4aaa036d36
Fahao Chen <chenfh612@gmail.com>
2021-11-23 20:46:32 +0900

    update

--- trainer.py
+++ trainer.py
@@ -196,13 +196,6 @@ class Trainer():
 			time_end = time.time()
 			time_cost_forward += time_end - time_start
 
-			# backward
-			time_start_back = time.time()
-			if grad:
-				self.optim_step(loss)
-			time_end_back = time.time()
-			time_cost_back += time_end_back - time_start_back
-
 			# release the GPU
 			for i, adj in enumerate(s.hist_adj_list):
 				s.hist_adj_list[i].to('cpu')
@@ -225,14 +218,27 @@ class Trainer():
 			# 	self.logger.log_minibatch(predictions, s.label_sp['vals'], loss.detach())
 			Loss.append(loss)
 		loss = sum(Loss)/len(Loss)
+		# backward
+		time_start_back = time.time()
+		if grad:
+			self.optim_step(loss)
+		time_end_back = time.time()
+
+		time_cost_back += time_end_back - time_start_back
+		time_may_com_1 = time.time()
 
 		time_total_end = time.time()
+		time_may_com_2 = time.time()
 		print('forwarding graphs: ',time_cost_forward)
 		print('backwarding graphs: ',time_cost_back)
 		print('processing graphs: ',time_total_end - time_total_start)
 		time_other_start = time.time()
 		torch.set_grad_enabled(True)
 		time_other_end = time.time()
+		time_may_com_3 = time.time()
+		print('may communication time 1: ', time_may_com_1 - time_end_back)
+		print('may communication time 2: ', time_may_com_2 - time_end_back)
+		print('may communication time 3: ', time_may_com_3 - time_end_back)
 		print('other time: ', time_other_end - time_other_start)
 		# precision, recall, f1 = self.logger.log_epoch_done()
 		if set_name=='TEST':
