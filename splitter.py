e3313c08546f11196f1025480bdf20de5b8a86ee
Fahao Chen <chenfh612@gmail.com>
2021-11-23 19:08:37 +0900

    update

--- splitter.py
+++ splitter.py
@@ -71,7 +71,7 @@ class splitter():
             end = int(length.item()) + start
             train = data_split(tasker, start, end, test = False)
             train = DataLoader(train,**args.data_loading_params)
-            # print(start,end)
+            print(start,end)
             # dev
             start = int(np.floor(train_total)) + args.num_hist_steps
             end = args.dev_proportion
@@ -81,7 +81,7 @@ class splitter():
             else:
                 dev = data_split(tasker, start, end, test = True)
             dev = DataLoader(dev,num_workers=args.data_loading_params['num_workers'])
-            # print(start,end)
+            print(start,end)
             # test
             start = end
             #the +1 is because I assume that max_time exists in the dataset
@@ -91,7 +91,7 @@ class splitter():
             else:
                 test = data_split(tasker, start, end, test = True)
             test = DataLoader(test,num_workers=args.data_loading_params['num_workers'])
-            # print(start,end)
+            print(start,end)
             print ('Dataset splits sizes:  train',len(train), 'dev',len(dev), 'test',len(test))
 
             self.tasker = tasker
