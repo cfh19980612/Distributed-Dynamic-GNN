53a1c2af9c3d95f1700c6641d5d626fc465e4832
Fahao Chen <chenfh612@gmail.com>
2021-12-03 00:13:01 +0900

    update

--- Distributed_SC/main.py
+++ Distributed_SC/main.py
@@ -213,7 +213,7 @@ def worker(rank, args, dataset, tasker):
 			# build gcn
 			GCN[rank] = build_gcn(args, tasker, rank)
 			# initialize the rpc group
-			rpc_backend_options.set_devices('cuda:0')
+			rpc_backend_options.set_devices("cuda:0")
 			rpc_backend_options.set_device_map('trainer1',{rank: rank + 1})
 			# options = TensorPipeRpcBackendOptions(
 			# 			init_method= "tcp://localhost:12349",
