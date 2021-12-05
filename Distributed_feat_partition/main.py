3e22b1d99bf03f17d9eb71117b5c0d24c4c34813
Fahao Chen <chenfh612@gmail.com>
2021-12-05 21:29:50 +0900

    update

--- Distributed_feat_partition/main.py
+++ Distributed_feat_partition/main.py
@@ -257,7 +257,7 @@ def main(args):
 
 	# build dataset
 	dataset = build_dataset(args)
-	dataset.max_time = dataset.max_time - 40
+	dataset.max_time = dataset.max_time
 	print('dataset complete!', dataset.num_nodes, dataset.max_time)
 
 	if args.distributed:
